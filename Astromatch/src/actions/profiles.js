4bd14f95a8ee06cf3698af88d633079b7eaddadf
MacksonWelton <mackson.welton@gmail.com>
2020-07-15 10:45:32 -0300

    Limpa a store quando é feito deslike

--- Astromatch/src/actions/profiles.js
+++ Astromatch/src/actions/profiles.js
@@ -9,6 +9,8 @@ export const clearSwipes = () => async (dispatch) => {
 export const getProfileToSwipe = () => async (dispatch) => {
 	const response = await axios.get('https://us-central1-missao-newton.cloudfunctions.net/astroMatch/mackson/person')
 
+	dispatch(getProfileData(undefined))
+
 	dispatch(getProfileData(
 		response.data.profile
 	))
